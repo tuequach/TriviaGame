44e094b607f90c98a09d6a5254c9cd0f496b0914
tuequach <tuequach@gmail.com>
2019-09-18 17:21:25 -0500

    adding in reset.css file and rewritting javascript file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,183 +1,137 @@
-$(document).ready(function(){
-var  trivia = [
-        {
-        question: "Who claims that he has worked on the Human Genome Project?",
-        choice: ["Eugene", "Abraham", "Gareth", "Rosita"],
-        answer: 0,
-        photo : "assets/images/genome.jpg"
-        },
-        {
-        question: "Quote the person that said this 'In this life now, you kill or you die. Or you die and you kill'?",
-        choice: ["Gareth", "Shane", "Rick", "The Govenor"],
-        answer: 3,
-        photo : "assets/images/lifeordeath.jpg"
-       },
-       {
-        question: "Who performs a C-section on Lori?",
-        choice: ["Carol", "Hershel", "Beth", "Maggie"],
-        answer: 3,
-        photo : "assets/images/csection.jpg"
-       },
-       {
-        question: "Which game do Beth and Daryl play after a harrowing day at a country club?",
-        choice: ["Never Have I ever", "Two Truths and A Lie", "Truth or Dare", "Twenty Questions"],
-        answer: 0,
-        photo : "assets/images/games.jpg"
-       },
-       {
-        question: "What is the name of the Govenor's Daughter?",
-        choice: ["Penny", "Abby", "Lizzy", "Kerry"],
-        answer: 0,
-        photo : "assets/images/daughter.jpg"
-       },
-       {
-        question: "In season 1, a doctor says which country almost found a cure for the virus?",
-        choice: ["USA", "Australia", "Japan", "France"],
-        answer: 3,
-        photo : "assets/images/cure.jpg"
-       },
-       {
-        question: "Which actor has a titanium eye socket in real life?",
-        choice: ["Steven Yeun", "Andrew Lincoln", "Norman Reedus", "Jeffrey Dean Morgan"],
-        answer: 2,
-        photo : "assets/images/titanium.jpg"
-       },
-       {
-        question: "What fictional place is Rick originally from?",
-        choice: ["Texas", "Georgia", "New York", "Neveda"],
-        answer: 1,
-        photo : "assets/images/rick.jpg"
-       }];
-    
-var  correct = 0;
-var  incorrect = 0;
-var  notAnswered = 0;
-var  running = false;
-var questionCount = trivia.length;
-var index;
-var  timer = 15;
-var  intervalId;
-var  userGuess ="";
-var  pick;
-var  holder = [];
-var  newArray =[];
-
-
-    //setting event listeners
-$("#reset").hide();
-   //on click to start the game
-$("#play").on("click", function () {
-       $("#play").hide();
-       displayQuestion();
-       runTimer();
-       for (var  i=0; i < trivia.length; i++) {
-           holder.push(trivia[i]);
-       }
-   })
-
-   //starting timer
-function runTimer () {
-       if (!running) {
-           intervalId = setInterval (decrement, 2000);
-           running = true;
-       }
-   }
-
-   //timer counting down 
-function decrement () {
-       $("#time").html("<h3>Time remaining: " + timer + "</h3>");
-       timer --;
-
-       //if time reaches 0
-       if (timer === 0) {
-           notAnswered++;
-           stop();
-           $("#answer").html("<p>Time's Up!, The correct answer is: " + pick.choice[pick.answer]  + "</p>");
-           hidepicture();
-       }
-   }
-
-
-   //stopping timer 
-function stop () {
-       running = false;
-       clearInterval(intervalId);
-   }
-
-// questions picked randomly from array?
-function displayQuestion () {
-    index = Math.floor(Math.random()*trivia.length);
-    pick = trivia[index];
-
-    $("#question").html("<h2>" + pick.question + "</h2>");
-    for (var  i =0; i <pick.choice.length; i++) {
-        var userChoice = $("<div>");
-        userChoice.addClass("answerchoice");
-        userChoice.html(pick.choice[i]);
-        userChoice.attr("data-guess", i);
-        $("#answer").append(userChoice);
+//setting up array for the trivia
+var MCU = [
+    {
+    question:"How many Infinity Stones are there in the Marvel Cinnematic Unniverse?",
+    choice: ["Ten", "Five", "Six" ,"Four"],
+    answer: 1, 
+    image: assets/images/,
+    },
+    {
+    question: "In Thor, What kind of doctor is Jane Foster?",
+    choice: ["Geologist", "Astrophysicist", "Surgeon", "Psychologist"],
+    answer: 1,
+    image: assets/images/,
+    },
+    {
+    question: "What is the name of Star-Lord's ship in Guardians of the Galaxy?",
+    choice: ["Freedom", "Firebird", "Jefferson Starship", "Milano"],
+    answer: 3,
+    image: assets/images/,
+    },
+    {
+    question: "What movie did Thanos first appear in?",
+    choice: ["Marvel's The Avengers", "Avengers: Age of Ultron", "Guardians of the Galaxy", "Iron Man 2"],
+    answer: 0,
+    image: assets/images/,
+    },
+    {
+    question: "How many versions of the Iron Man armor has Tony Stark made?",
+    choice: ["12", "30", "24", "50"],
+    answer: 3,
+    image: assets/images/,
+    },
+    {
+    question: "What is the name of Tony Stark's personal computer butler?",
+    choice: ["Jennings", "Alfred", "Jarvis", "Jeeves"],
+    answer: 2,
+    image: assets/images/,
+    },
+    {
+    question: "What material is Captain America's shield made of?",
+    choice: ["Adamantium", "Kryptonite", "Tritanium", "Vibranium"],
+    answer: 3,
+    image: assets/images/,
+    },
+    {
+    question: "What was Dr. Strange's procession before he become Sorcerer Supreme?",
+    choice: ["Dentist", "Neurosurgeon", "Dermatologist", "Therapist"],
+    answer: 1,
+    image: assets/images/,
+    },
+    {
+    question: "Who does Bruce Banner become?",
+    choice: ["Batman", "Spiderman", "Hulk", "Captain America"],
+    answer: 2,
+    image: assets/images/,
+    },
+    {
+    question: "Who is Tony Stark's best friend, and also known as War Machine?",
+    choice: ["James Rhodes", "Pepper Potts", "Happy Hogan", "Peter Parker"],
+    answer: 0,
+    image: assets/images/,
+    },
+    {
+    question: "Who is Peter Parker's well known archenemy in most of Spider-Man comics?",
+    choice: ["The Sandman" , "The Green Goblin", "Doctor Octopus", "Carnage"],
+    answer: 1,
+    image: assets/images/,
+    },
+    {
+    question: "What is the name of the somic cube that Loki takes control of in The Avengers?",
+    choice: ["The Heart of Gaia", "The Magic Cube", "The Tesseract", "The Orb"],
+    answer: 2,
+    image: assets/images/,
+    },
+    {
+    question: "What is the name of Tony Stark's company?",
+    choice: ["Stark Industries", "Stark and Co", "Stark and Sons", "Stark International"],
+    answer: 0,
+    image: assets/images/,
+    },
+    {
+    question: "How many years was Captain America frozen for in Captain America: The First Avenger?",
+    choice: ["50 years", "70 years", "100 years", "200 years"],
+    answer: 2,
+    image: assets/images/,
+    },
+    {
+    question: "What is the name of the planet that Thor and Loki come from?",
+    choice: ["Xandar", "Asgard", "Sakaar", "Jötunheim"],
+    answer: 1,
+    image: assets/images/,
     }
+];
 
-//on click that shows outcome for selected answer
+//initializing all global variables 
+var correctAnswer;
+var incorrectAnswer;
+var unanswered;
+var answered;
+var seconds;
+var time;
+var currentQuestion;
+var selected;
 
-$(".answerChoice").on("click", function () {
-        userGuess = parseInt($(this).attr("data-guess"));
-
-        if (userGuess === pick.answer) {
-            stop ();
-            correct++;
-            userGuess ="";
-            $("#answer").html("<p> Correct!</p>");
-            hidepicture();
-        }
-        else {
-            stop();
-            incorrect++;
-            userGuess="";
-            $("#answer").html("<p>Wrong! The correct answer is: " + pick.choice[pick.answer] + "</p>");
-            hidepicture ();
-            }
-        })
-        }
-
-function hidepicture () {
-    $("#answer").append("<img src=" + pick.photo + ">");
-    newArray.push(pick);
-    trivia.splice(index,1);
-
-    var  hiddenPic = setTimeout (function () {
-        $("#answer").empty();
-        timer=15;
-
-    if ((incorrect + correct + notAnswered) === questionCount) {
-        $("#question").empty();
-		$("#question").html("<h3>Game Over!  Here's how you did: </h3>");
-		$("#answer").append("<h4> Correct: " + correct + "</h4>" );
-		$("#answer").append("<h4> Incorrect: " + incorrect + "</h4>" );
-		$("#answer").append("<h4> Unanswered: " + notAnswered + "</h4>" );
-		$("#reset").show();
-		correct = 0;
-		incorrect = 0;
-        notAnswered = 0;
-    
-    } 
-    else {
-		runTimer();
-		displayQuestion();
-}
-	}, 3000);
+var messages = { 
+    correct: "Correct!",
+    incorrect: "Incorrect! The answer is " + correctAnswer,
+    endTime: " Out of time!", 
+    finished: "Lets see your final score and how well you know the answer"
 }
 
+//starting the game with start button
+$("#start").on("click", function() {
+    $(this).hide();
+    newGame();
+});
+
+//reset button to replay/ new game
 $("#reset").on("click", function() {
-	$("#reset").hide();
-	$("#answer").empty();
-	$("#question").empty();
-	for(var i = 0; i < holder.length; i++) {
-		trivia.push(holder[i]);
-	}
-	    runTimer();
-	    displayQuestion();
+    $(this).hide();
+    newGame();
+});
 
-})
+function newGame(){
+    $("#correctAnswer").empty();
+    $("#incorrectAnswer").empty();
+    $("#unanswered").empty();
+    currentQuestion = 0;
+    correctAnswer =0;
+    incorrectAnswer =0;
+    unanswered =0;
+    newQuestion();
+}
 
-})
+function newQuestion() {
+    
+}
